9e99d739e11ad6630b67390e1a025b05308eb1ad
meldeleon <melissadeleon@gmail.com>
2022-11-30 21:30:07 -0800

    day 1 2022 solutions

---
+++ butts.js
@@ -0,0 +1,6 @@
+let butts = ["22", ""]
+if (butts[1]) {
+  console.log("truthy")
+} else {
+  console.log("falsy")
+}
---
+++ day01_pt2.js
@@ -0,0 +1,31 @@
+const input = require("fs")
+  .readFileSync("day01_input.txt")
+  .toString()
+  .split(/\r?\n/)
+// console.log(input)
+let intMap = input.map((x) => {
+  if (x === "") {
+    return false
+  } else {
+    return parseInt(x)
+  }
+})
+// console.log(intMap)
+
+let elfTotals = []
+
+let runningTotal = 0
+for (let i = 0; i < intMap.length; i++) {
+  if (intMap[i] !== false) {
+    runningTotal += intMap[i]
+  } else {
+    elfTotals.push(runningTotal)
+    runningTotal = 0
+  }
+}
+
+//console.log(elfTotals)
+
+let sorted = elfTotals.sort((a, b) => b - a)
+let topThree = sorted[0] + sorted[1] + sorted[2]
+console.log(`the answer is ${topThree}`)
