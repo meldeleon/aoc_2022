58cdb809f9c5d7c3b857681d4eff44e08d71e682
meldeleon <melissadeleon@gmail.com>
2022-12-01 20:34:01 -0800

    adding day 2 files

---
+++ day02.js
@@ -0,0 +1,5 @@
+const input = require("fs")
+  .readFileSync("day02_input.txt")
+  .toString()
+  .split(/\r?\n/)
+//console.log(input)
---
+++ day02.py
@@ -0,0 +1,2 @@
+with open('day02_input.txt') as f:
+    chunks = f.read().split("\n\n")
